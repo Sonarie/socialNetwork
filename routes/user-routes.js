745b0374475c26614991c3668926f51d98881be4
Sonarie <sonarie1980@gmail.com>
2022-05-01 15:03:37 -0500

    user routes done

---
+++ routes/user-routes.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const {
+  getAllUser,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+} = require("../controllers/user-controllers");
+
+//Set up Get all and Post at /api/users
+router.route("/").get(getAllUser).post(createUser);
+
+//Set up Get one, PUT, and DELETE at /api/pizzas/:id
+router.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
+
+module.exports = router;
