c830f7c652ebdb28d053630ed6b7103b37f27bed
Sonarie <sonarie1980@gmail.com>
2022-05-01 17:45:38 -0500

    add reaction model

---
+++ models/reaction.js
@@ -0,0 +1,37 @@
+const ReactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      validate: {
+        validator: "isLength",
+        arguments: [280],
+        message: "Text should be a minimum of 280 characters only",
+      },
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+const Reaction = model("Reaction", ReactionSchema);
+
+module.exports = Reaction;
--- models/thought.js
+++ models/thought.js
@@ -1,71 +1,41 @@
 const { Schema, model, Types } = require("mongoose");
 const validate = require("mongoose-validator");
-const dateFormat = require("../utils/dateFormat");
+const dateFormat = require("../public/utils/dateFormat");
+const ReactionSchema = require("./reaction");
 
-const ThoughtSchema = new Schema({
-  thoughtText: {
-    type: String,
-    required: true,
-    validate: {
-      validator: "isLength",
-      arguments: [1, 280],
-      message: "Text should be between 1 and 280 characters only",
+const ThoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      validate: {
+        validator: "isLength",
+        arguments: [1, 280],
+        message: "Text should be between 1 and 280 characters only",
+      },
     },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [ReactionSchema],
   },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    get: (createdAtVal) => dateFormat(createdAtVal),
-  },
-  username: {
-    type: String,
-    required: true,
-  },
-  reactions: [ReactionSchema],
-},
-{
-  toJSON: {
-    getters: true,
-  },
-}
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
 );
 
-const ReactionSchema = new Schema({
-  reactionId: {
-    type: Schema.Types.ObjectId,
-    default: () => new Types.ObjectId(),
-  },
-  reactionBody: {
-    type: String,
-    required: true,
-    validate: {
-      validator: "isLength",
-      arguments: [280],
-      message: "Text should be a minimum of 280 characters only",
-    },
-  },
-  username: {
-    type: String,
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    get: (createdAtVal) => dateFormat(createdAtVal),
-  },
-},
-{
-  toJSON: {
-    virtuals: true,
-    getters: true,
-  },
-  id: false,
+ThoughtSchema.virtual("reactionCount").get(function () {
+  return this.replies.length;
 });
 
-ThoughtSchema.virtual("reactionCount").get(function () {
-    return this.replies.length;
-});
-  
 const Thought = model("Thought", ThoughtSchema);
-  
+
 module.exports = Thought;
